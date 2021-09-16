12aa6e4447ee7ff2d917f7e7cebaa0d2ed57467d
Swasthik <swasthik.ks22@gmail.com>
2021-09-16 22:52:38 +0530

    Validate all conditions for email

--- src/email/Email.java
+++ src/email/Email.java
@@ -6,11 +6,10 @@ public class Email {
 
 	public static void main(String[] args) {
 		
-		String pattern = "[a-zA-Z][0-9a-zA-Z]*([+.-_][0-9a-zA-Z]+)?\\@[a-zA-Z]+\\.[a-zA-Z]+";
+		String pattern = "[a-zA-Z][0-9a-zA-Z]*([.\\-_+][0-9a-zA-Z]+)?\\@[0-9a-zA-Z]+\\.[a-zA-Z]+([.][a-z]{2})?";
 		
-		String email = "abc.xyz@bridgelabz.com";
+		String email = "abc.xyz@bridgelabz.com.in";
 		
 		System.out.println(email + " is " + (Pattern.matches(pattern, email)?"Valid":"Invalid"));
 	}
-
 }
