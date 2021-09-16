db5f14df513b9c92f3a3c9a41cb3569c69663767
Swasthik <swasthik.ks22@gmail.com>
2021-09-16 22:30:47 +0530

    Validate optional part

--- src/email/Email.java
+++ src/email/Email.java
@@ -6,9 +6,9 @@ public class Email {
 
 	public static void main(String[] args) {
 		
-		String pattern = "[a-zA-Z][0-9a-zA-Z]*\\@[a-zA-Z]+\\.[a-zA-Z]+";
+		String pattern = "[a-zA-Z][0-9a-zA-Z]*([+.-_][0-9a-zA-Z]+)?\\@[a-zA-Z]+\\.[a-zA-Z]+";
 		
-		String email = "swz20@gmail.com";
+		String email = "abc.xyz@bridgelabz.com";
 		
 		System.out.println(email + " is " + (Pattern.matches(pattern, email)?"Valid":"Invalid"));
 	}
