8f47cd56553a533ef0af280a827c41e412d7aac8
Swasthik <swasthik.ks22@gmail.com>
2021-09-16 21:44:31 +0530

    Validate first part of Email

---
+++ src/email/Email.java
@@ -0,0 +1,16 @@
+package email;
+
+import java.util.regex.*;
+
+public class Email {
+
+	public static void main(String[] args) {
+		
+		String pattern = "[a-zA-Z][0-9a-zA-Z]*";
+		
+		String email = "swz20";
+		
+		System.out.println(email + " is " + (Pattern.matches(pattern, email)?"Valid":"Invalid"));
+	}
+
+}
